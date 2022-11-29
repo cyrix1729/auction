933391c39b1f1d3fa7d98629e375c72142509b26
Faris J <ec20605@qmul.ac.uk>
2022-11-29 02:04:19 +0000

    added all views

--- auction/auction/urls.py
+++ auction/auction/urls.py
@@ -3,5 +3,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path('login/', include('login.urls')),
+    path('item/', include('item.urls')),
+    path('listings/', include('listings.urls')),
+    path('profile/', include('profile.urls')),
+    path('list/', include('list.urls')),
+
     path('admin/', admin.site.urls),
 ]
